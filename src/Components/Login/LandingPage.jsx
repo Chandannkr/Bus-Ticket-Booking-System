60645311d43693a3ea4bb5f6625c82585fdb2fa7
Chandan kr <chandan.sranjan@gmail.com>
2025-01-29 15:40:42 +0530

    update commit

--- src/Components/Login/LandingPage.jsx
+++ src/Components/Login/LandingPage.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import '../../Styles/LandingPage.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "../../Styles/LandingPage.css";
 
 export default function LandingPage() {
- 
-    return (
-    <div className='LandingPage'>
+  return (
+    <div className="LandingPage">
       <h1>Welcome To Bus Ticket Booking System</h1>
-          <p>
-            This is a simple Bus Ticket Booking System. You can login as an Admin or User. 
-          </p> 
-        <div className="container">
-          <Link to='/adminlogin'>Admin Login/SignUp</Link> <br />
-          <Link to='/userlogin'>User LoginSignUp</Link>
-        </div>
-        <div className="content">
+      <p>
+        This is a simple Bus Ticket Booking System. You can login as an Admin or
+        User.
+      </p>
+      <div className="container">
+        <Link to="/adminlogin">Admin Login/SignUp</Link> <br />
+        <Link to="/userlogin">User LoginSignUp</Link>
+      </div>
+      <div className="content">
         <div className="scrolling-text">
-            <h1>India's No. 1 Online Bus Ticket Booking Site</h1>
-          </div>
-          <p>Book bus tickets online from the largest bus ticket booking site with the best bus operators. Book your bus ticket now!</p>
+          <h1>India's No. 1 Online Bus Ticket Booking Site</h1>
         </div>
+        <p>
+          Book bus tickets online from the largest bus ticket booking site with
+          the best bus operators. Book your bus ticket now!
+        </p>
+
+        <div className="container">
+          {/* <div className="text">
+            <h3>Book Bus Ticket</h3>
+          </div> */}
+          <div className="search_container">
+            <div className="search_box">
+              <input type="text" placeholder="Source" />
+              <input type="text" placeholder="Destination" />
+              <div className="date_container">
+                <input type="date" />
+                <button>Today</button>
+                <button>Tomorrow</button>
+              </div>
+              <button>Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
