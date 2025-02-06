23dafae47642009f8dbe10832b90afc8fdd9f4b8
Chandan kr <chandan.sranjan@gmail.com>
2025-02-06 17:33:09 +0530

    updated commit

--- src/Components/Main/AdminHomePage.jsx
+++ src/Components/Main/AdminHomePage.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import Header from '../../Components/Main/Header'
-import AdminSideBar from '../../Styles/AdminSideBar.css'
+import React from "react";
+import Header from "../../Components/Main/Header";
+import AdminSideBar from "../../Styles/AdminSideBar.css";
+import { Route, Routes } from "react-router-dom";
+import AdminDashboard from "../../Components/Main/AdminDashboard";
+import AddBus from "../../Components/Main/AddBus";
 
 const AdminHomePage = () => {
-    return (
-        <div>
-            {/* <h1>Welcome to the Admin Home Page</h1>
-            <p>This is the admin section of the cart app.</p> */}
-            <Header/>
-            <section>
-                <aside className='side_content'>
-                <AdminSideBar/>
-                </aside>
-                <aside className='content'>Content</aside>
-            </section>
-        </div>
-    );
+  return (
+    <div className="AdminHomePage">
+      <Header />
+      <section>
+        <aside className="side_content">
+          <AdminSideBar />
+        </aside>
+        <aside className="content">
+          <Routes>
+            <Route path="/dashboard" element={<AdminDashboard />} />
+            <Route path="/addbus" element={<AddBus />} />
+          </Routes>
+        </aside>
+      </section>
+    </div>
+  );
 };
 
 export default AdminHomePage;
