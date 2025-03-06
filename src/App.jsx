e11f560b655e37695d3de076e0723cd86f76d9bc
Chandan Kumar <chandan.sranjan@gmail.com>
2025-03-06 18:29:32 +0530

    updated commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import "./App.css";
 
 import LandingPage from "./Components/Login/LandingPage";
@@ -13,7 +12,7 @@ import AdminHomePage from "./Components/Main/AdminHomePage";
 
 function App() {
   return (
-    <Router> {/* Removed 'basename' if it was set */}
+    <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
@@ -22,7 +21,7 @@ function App() {
         <Route path="/adminhomepage" element={<AdminHomePage />} />
       </Routes>
       <ToastContainer />
-    </Router>
+    </>
   );
 }
 
